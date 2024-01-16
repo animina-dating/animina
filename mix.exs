7920d08d197a5cc85b1a55f04e02c2ebee2acc33
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2024-01-16 11:08:12 +0100

    Adds credo static code analysis tool

--- mix.exs
+++ mix.exs
@@ -50,7 +50,8 @@ defmodule Animina.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 
--- setup-hooks.sh
+++ setup-hooks.sh
@@ -5,10 +5,31 @@ PRE_COMMIT_HOOK=".git/hooks/pre-commit"
 # Create the pre-commit hook
 cat > $PRE_COMMIT_HOOK <<EOF
 #!/bin/sh
+
+# Stash non-staged changes
 git stash -q --keep-index
+
+# Run mix format
 mix format
+
+# Add any formatting changes to the staging area
 git add -u
+
+# Run mix credo
+echo "Running mix credo..."
+mix credo --strict
+
+# Check if Credo exited with a non-zero status (indicating issues)
+if [ \$? -ne 0 ]; then
+  echo "Credo found issues, commit aborted!"
+  git stash pop -q
+  exit 1
+fi
+
+# Unstash changes stashed earlier
 git stash pop -q
+
+# Exit with success
 exit 0
 EOF
 
