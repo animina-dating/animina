57baf8e6692e4a9d9577751cff650027ec963664
Philipp Kempgen <office@kempgen.net>
2024-02-28 02:07:19 +0100

    seed hashtags in docker-entrypoint.sh (amends commit 79cdf1cb30918a5bfbb59eae48f781d29487585a from https://github.com/animina-dating/animina/pull/66)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,6 +7,9 @@ echo "Create the database and run migrations ..."
 mix ash_postgres.create
 mix ash_postgres.migrate
 
+echo "Seeding hashtags ..."
+mix run priv/repo/flag_seeds.exs || true    #TODO No need to run every time. Should these go into a migration?
+
 echo "Starting application ..."
 mix phx.server
 #iex -S mix phx.server
