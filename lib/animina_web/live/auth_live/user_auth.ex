66dacae2cebd808dd4a8f3ac7734ddfb8630842e
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2024-04-18 16:44:14 +0300

    added the extra 150 points if user has used the system.

--- lib/animina_web/live/auth_live/user_auth.ex
+++ lib/animina_web/live/auth_live/user_auth.ex
@@ -5,6 +5,7 @@ defmodule AniminaWeb.LiveUserAuth do
 
   alias AniminaWeb.Registration
   alias Animina.Accounts.Credit
+  alias Animina.Accounts.Points
   import Phoenix.Component
 
   use AniminaWeb, :verified_routes
@@ -74,7 +75,9 @@ defmodule AniminaWeb.LiveUserAuth do
   end
 
   def check_if_user_has_daily_bonus_added_for_the_day(user_id) do
-    Credit.read!()
+    {:ok, credits} = Credit.read()
+
+    credits
     |> Enum.filter(fn credit ->
       credit.user_id == user_id and
         format_date(credit.created_at) == format_date(DateTime.utc_now()) and
@@ -95,8 +98,35 @@ defmodule AniminaWeb.LiveUserAuth do
       points: points,
       subject: "Daily Bonus"
     })
+
+    # we then check if a user has been using the system for 10 days straight if so ,
+    # we add 150 extra points so that they can have a total of 250 points
+
+    if Points.has_daily_bonus_for_the_past_ten_days(
+         daily_bonus_credits_for_a_user(user.id),
+         current_day()
+       ) do
+      Credit.create(%{
+        user_id: user.id,
+        points: 150,
+        subject: "Daily Bonus"
+      })
+    end
   end
 
   defp add_daily_points_for_user(_user, _points, _) do
+  end
+
+  def current_day do
+    Timex.now()
+  end
+
+  def daily_bonus_credits_for_a_user(user_id) do
+    {:ok, credits} = Credit.read()
+
+    credits
+    |> Enum.filter(fn credit ->
+      credit.user_id == user_id and credit.subject == "Daily Bonus"
+    end)
   end
 end
