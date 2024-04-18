0faf5ca88dd1704c30a64242e3fa66a99dca3845
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2024-04-18 15:24:26 +0300

    user gets 100 bonus points every time they use the system

--- lib/animina_web/live/auth_live/user_auth.ex
+++ lib/animina_web/live/auth_live/user_auth.ex
@@ -4,6 +4,7 @@ defmodule AniminaWeb.LiveUserAuth do
   """
 
   alias AniminaWeb.Registration
+  alias Animina.Accounts.Credit
   import Phoenix.Component
 
   use AniminaWeb, :verified_routes
@@ -46,6 +47,12 @@ defmodule AniminaWeb.LiveUserAuth do
     if socket.assigns[:current_user] do
       current_user = Registration.get_current_user(session)
 
+      add_daily_points_for_user(
+        current_user,
+        100,
+        check_if_user_has_daily_bonus_added_for_the_day(current_user.id)
+      )
+
       {:cont,
        socket
        |> assign(:current_user, current_user)
@@ -64,5 +71,32 @@ defmodule AniminaWeb.LiveUserAuth do
        |> assign(:current_user_credit_points, 0)
        |> assign(:current_user, nil)}
     end
+  end
+
+  def check_if_user_has_daily_bonus_added_for_the_day(user_id) do
+    Credit.read!()
+    |> Enum.filter(fn credit ->
+      credit.user_id == user_id and
+        format_date(credit.created_at) == format_date(DateTime.utc_now()) and
+        credit.subject == "Daily Bonus"
+    end)
+    |> List.first()
+  end
+
+  defp format_date(date) do
+    {:ok, date} = Timex.format(date, "{YYYY}-{0M}-{0D}")
+    date
+  end
+
+  # in this case the user does not have a daily bonus added for the day , so we add one
+  defp add_daily_points_for_user(user, points, nil) do
+    Credit.create(%{
+      user_id: user.id,
+      points: points,
+      subject: "Daily Bonus"
+    })
+  end
+
+  defp add_daily_points_for_user(_user, _points, _) do
   end
 end
