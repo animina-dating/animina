52243bd1894f990c53a0e93be22e2570c811d472
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2024-04-27 11:34:22 +0300

    added bad username validation

--- test/animina/accounts/user_test.exs
+++ test/animina/accounts/user_test.exs
@@ -25,5 +25,22 @@ defmodule Animina.Accounts.UserTest do
 
       assert {:ok, _} = User.by_email("bob@example.com")
     end
+
+    test "does not create a user if they have a bad username" do
+      assert {:error, _} =
+               BasicUser.create(%{
+                 email: "name@example.com",
+                 username: "my",
+                 name: "Bobby",
+                 hashed_password: "zzzzzzzzzzz",
+                 birthday: "1950-01-01",
+                 height: 180,
+                 zip_code: "56068",
+                 gender: "male",
+                 mobile_phone: "0151-12315678",
+                 language: "de",
+                 legal_terms_accepted: true
+               })
+    end
   end
 end
