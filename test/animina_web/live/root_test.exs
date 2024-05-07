5d49cb490622a2ef67a4c1f18ef055c60b7759fd
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2024-05-07 14:46:49 +0300

    added redirect to "/my/potential-partner"

--- test/animina_web/live/root_test.exs
+++ test/animina_web/live/root_test.exs
@@ -1,13 +1,12 @@
 defmodule AniminaWeb.RootTest do
   use AniminaWeb.ConnCase
   import Phoenix.LiveViewTest
-  alias Animina.Accounts.User
 
   @valid_attrs %{
     email: "michael@example.com",
     username: "MichaelMunavu",
     name: "Michael",
-    hashed_password: Bcrypt.hash_pwd_salt("Michael123"),
+    password: "MichaelTheEngineer",
     birthday: "1950-01-01",
     height: 180,
     zip_code: "56068",
@@ -49,20 +48,18 @@ defmodule AniminaWeb.RootTest do
          %{conn: conn} do
       {:ok, _view, _html} = live(conn, "/")
 
-      {:ok, user} = User.create(@valid_attrs)
-
       {:ok, _index_live, html} =
-        conn |> login_user(user.email, "Michael123") |> live(~p"/my/potential-partner/")
+        conn |> sign_in_user(@valid_attrs) |> live(~p"/my/potential-partner/")
 
       assert html =~ "Criteria for your new partner"
     end
   end
 
-  defp login_user(conn, email, password) do
-    {:ok, lv, _html} = live(conn, ~p"/sign-in/")
+  defp sign_in_user(conn, attributes) do
+    {:ok, lv, _html} = live(conn, ~p"/")
 
     form =
-      form(lv, "#basic_user_sign_in_form", user: %{email: email, password: password})
+      form(lv, "#basic_user_form", user: attributes)
 
     submit_form(form, conn)
   end
