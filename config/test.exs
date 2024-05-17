6cad4d38ea1dbf5fd5ea7590bffb6b7b1031bf40
briankariuki <briankariukidev@gmail.com>
2024-05-17 19:06:20 +0300

    change pool_size, queue_target config

--- config/test.exs
+++ config/test.exs
@@ -11,7 +11,8 @@ config :animina, Animina.Repo,
   hostname: "localhost",
   database: "animina_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
-  pool_size: 10
+  pool_size: 20,
+  queue_target: 5000
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
