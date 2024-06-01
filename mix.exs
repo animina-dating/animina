3c2082c754c651b6c4a28862477855a156eb9d09
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2024-06-01 19:59:25 +0200

    CI Test

--- mix.exs
+++ mix.exs
@@ -96,10 +96,10 @@ defmodule Animina.MixProject do
   defp aliases do
     if System.get_env("CI") do
       [
-        setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
-        "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-        "ecto.reset": ["ecto.drop", "ecto.setup"],
-        test: ["ecto.migrate --quiet", "test"],
+        setup: ["deps.get", "assets.setup", "assets.build"],
+        "ecto.setup": [""],
+        "ecto.reset": [""],
+        test: ["test"],
         "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
         "assets.build": ["tailwind default", "esbuild default"],
         "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"],
