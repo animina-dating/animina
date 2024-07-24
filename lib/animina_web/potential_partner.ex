09b6758b41ceee75ef4d6cbe060cf20dca2fa799
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2024-07-24 11:27:41 +0200

    Fixes warnings

--- lib/animina_web/potential_partner.ex
+++ lib/animina_web/potential_partner.ex
@@ -29,7 +29,7 @@ defmodule AniminaWeb.PotentialPartner do
   """
   def potential_partners(user, options \\ []) do
     limit = Keyword.get(options, :limit, 10)
-    strict_red_flags = Keyword.get(options, :strict_red_flags, false)
+    # strict_red_flags = Keyword.get(options, :strict_red_flags, false)
     remove_bookmarked = Keyword.get(options, :remove_bookmarked_potential_users, true)
 
     User
@@ -94,107 +94,107 @@ defmodule AniminaWeb.PotentialPartner do
     |> Ash.Query.filter(id not in ^bookmarked_users)
   end
 
-  defp partner_height_query(query, user) do
-    query
-    |> Ash.Query.filter(
-      or: [
-        height: [less_than_or_equal: user.maximum_partner_height],
-        height: [greater_than_or_equal: user.minimum_partner_height]
-      ]
-    )
-  end
+  # defp partner_height_query(query, user) do
+  #   query
+  #   |> Ash.Query.filter(
+  #     or: [
+  #       height: [less_than_or_equal: user.maximum_partner_height],
+  #       height: [greater_than_or_equal: user.minimum_partner_height]
+  #     ]
+  #   )
+  # end
 
   # We use a fragment query to calculate the age of the user as
   # ash does not support using module calculations defined with calculate/3 in filter queries
-  defp partner_age_query(query, user) do
-    conditional_partner_age_query(query, user.minimum_partner_age, user.maximum_partner_age)
-  end
+  # defp partner_age_query(query, user) do
+  #   conditional_partner_age_query(query, user.minimum_partner_age, user.maximum_partner_age)
+  # end
 
-  defp conditional_partner_age_query(query, nil, nil) do
-    query
-  end
+  # defp conditional_partner_age_query(query, nil, nil) do
+  #   query
+  # end
 
-  defp conditional_partner_age_query(query, minimum_partner_age, nil) do
-    query
-    |> Ash.Query.filter(
-      fragment("date_part('year', age(current_date, ?))", birthday) >= ^minimum_partner_age
-    )
-  end
+  # defp conditional_partner_age_query(query, minimum_partner_age, nil) do
+  #   query
+  #   |> Ash.Query.filter(
+  #     fragment("date_part('year', age(current_date, ?))", birthday) >= ^minimum_partner_age
+  #   )
+  # end
 
-  defp conditional_partner_age_query(query, nil, maximum_partner_age) do
-    query
-    |> Ash.Query.filter(
-      fragment("date_part('year', age(current_date, ?))", birthday) <= ^maximum_partner_age
-    )
-  end
+  # defp conditional_partner_age_query(query, nil, maximum_partner_age) do
+  #   query
+  #   |> Ash.Query.filter(
+  #     fragment("date_part('year', age(current_date, ?))", birthday) <= ^maximum_partner_age
+  #   )
+  # end
 
-  defp conditional_partner_age_query(query, minimum_partner_age, maximum_partner_age) do
-    query
-    |> Ash.Query.filter(
-      fragment("date_part('year', age(current_date, ?))", birthday) <= ^maximum_partner_age and
-        fragment("date_part('year', age(current_date, ?))", birthday) >= ^minimum_partner_age
-    )
-  end
+  # defp conditional_partner_age_query(query, minimum_partner_age, maximum_partner_age) do
+  #   query
+  #   |> Ash.Query.filter(
+  #     fragment("date_part('year', age(current_date, ?))", birthday) <= ^maximum_partner_age and
+  #       fragment("date_part('year', age(current_date, ?))", birthday) >= ^minimum_partner_age
+  #   )
+  # end
 
   defp partner_gender_query(query, user) do
     query
     |> Ash.Query.filter(gender: [eq: user.partner_gender])
   end
 
-  defp partner_geo_query(query, user) do
-    nearby_zip_codes =
-      get_nearby_cities(user.zip_code, user.search_range)
-      |> Enum.map(fn city -> city.zip_code end)
+  # defp partner_geo_query(query, user) do
+  #   nearby_zip_codes =
+  #     get_nearby_cities(user.zip_code, user.search_range)
+  #     |> Enum.map(fn city -> city.zip_code end)
 
-    query
-    |> Ash.Query.filter(zip_code: [in: nearby_zip_codes])
-  end
+  #   query
+  #   |> Ash.Query.filter(zip_code: [in: nearby_zip_codes])
+  # end
 
-  defp partner_green_flags_query(query, user) do
-    white_flags =
-      get_user_flags(user.id, :white)
-      |> Enum.map(fn flag -> flag.flag_id end)
+  # defp partner_green_flags_query(query, user) do
+  #   white_flags =
+  #     get_user_flags(user.id, :white)
+  #     |> Enum.map(fn flag -> flag.flag_id end)
 
-    query
-    |> Ash.Query.filter(traits.color == :green and traits.flag_id in ^white_flags)
-  end
+  #   query
+  #   |> Ash.Query.filter(traits.color == :green and traits.flag_id in ^white_flags)
+  # end
 
-  defp partner_red_flags_query(query, user, strict_red_flags) when strict_red_flags == true do
-    red_flags =
-      get_user_flags(user.id, :red)
-      |> Enum.map(fn flag -> flag.flag_id end)
+  # defp partner_red_flags_query(query, user, strict_red_flags) when strict_red_flags == true do
+  #   red_flags =
+  #     get_user_flags(user.id, :red)
+  #     |> Enum.map(fn flag -> flag.flag_id end)
 
-    query
-    |> Ash.Query.filter(traits.color == :white and traits.flag_id not in ^red_flags)
-  end
+  #   query
+  #   |> Ash.Query.filter(traits.color == :white and traits.flag_id not in ^red_flags)
+  # end
 
-  defp partner_red_flags_query(query, _user, _strict_red_flags) do
-    query
-  end
+  # defp partner_red_flags_query(query, _user, _strict_red_flags) do
+  #   query
+  # end
 
   # We use the haversine formula to get locations
   # near the current city in the search range radius
-  defp get_nearby_cities(zip_code, search_range) do
-    current_city = City.by_zip_code!(zip_code)
+  # defp get_nearby_cities(zip_code, search_range) do
+  #   current_city = City.by_zip_code!(zip_code)
 
-    City
-    |> Ash.Query.filter(
-      fragment(
-        "acos(sin(radians(?)) * sin(radians(lat)) + cos(radians(?)) * cos(radians(lat)) * cos(radians(?) - radians(lon))) * 6731 <= ?",
-        ^current_city.lat,
-        ^current_city.lat,
-        ^current_city.lon,
-        ^search_range
-      )
-    )
-    |> Ash.read!()
-  end
+  #   City
+  #   |> Ash.Query.filter(
+  #     fragment(
+  #       "acos(sin(radians(?)) * sin(radians(lat)) + cos(radians(?)) * cos(radians(lat)) * cos(radians(?) - radians(lon))) * 6731 <= ?",
+  #       ^current_city.lat,
+  #       ^current_city.lat,
+  #       ^current_city.lon,
+  #       ^search_range
+  #     )
+  #   )
+  #   |> Ash.read!()
+  # end
 
-  defp get_user_flags(user_id, color) do
-    UserFlags
-    |> Ash.Query.for_read(:by_user_id, %{id: user_id, color: color})
-    |> Ash.read!()
-  end
+  # defp get_user_flags(user_id, color) do
+  #   UserFlags
+  #   |> Ash.Query.for_read(:by_user_id, %{id: user_id, color: color})
+  #   |> Ash.read!()
+  # end
 
   defp get_bookmarked_users(user_id) do
     case Accounts.Bookmark.by_owner(user_id) do
