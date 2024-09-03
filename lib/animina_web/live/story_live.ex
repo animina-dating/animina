f9fedb9fc7393afbd14f137ccb23733640e335f9
Kimutai01 <kiprotichkimutai01@gmail.com>
2024-09-03 16:36:01 +0300

    story LLM bug fixes

--- lib/animina_web/live/story_live.ex
+++ lib/animina_web/live/story_live.ex
@@ -940,7 +940,7 @@ defmodule AniminaWeb.StoryLive do
             </div>
 
             <button
-              :if={length(@reasons) > 0 && @show_buttons == true}
+              :if={length(@reasons) > 0 && @show_buttons == true && f[:headline_id].value != nil}
               type="button"
               phx-click="generate_story"
               class="flex mt-5 justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
