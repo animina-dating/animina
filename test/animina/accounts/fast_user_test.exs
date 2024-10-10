0b987062b4867759e04d2c42a6ddba458acbf29c
briankariuki <briankariukidev@gmail.com>
2024-10-10 11:17:53 +0300

    add fast user resource tests

--- test/animina/accounts/fast_user_test.exs
+++ test/animina/accounts/fast_user_test.exs
@@ -1,33 +1,71 @@
 defmodule Animina.Accounts.FastUserTest do
   use Animina.DataCase, async: true
-  alias Animina.Accounts.Role
-  alias Animina.Accounts.BasicUser
-  alias Animina.Accounts.UserRole
+  alias Animina.Accounts.FastUser
   alias Animina.Accounts.Photo
+  alias Animina.Accounts.Role
+  alias Animina.Accounts.User
+  alias Animina.Accounts.UserRole
 
-  describe "Tests for the Fast User module" do
+  describe "Tests for the FastUser resource" do
     setup do
       user = create_user()
       role = create_admin_role()
-      create_user_role(role, user)
-      create_profile_picture(user)
+      user_role = create_user_role(role, user)
+      profile_photo = create_profile_picture(user)
 
       [
-        user: user
+        user: user,
+        user_role: user_role,
+        profile_photo: profile_photo
       ]
     end
 
-    test "The first story you can create is the 'About me' story",
+    test "Can read user by username",
+         %{
+           user: user,
+           profile_photo: profile_photo
+         } do
+      result =
+        FastUser
+        |> Ash.ActionInput.for_action(:by_id_email_or_username, %{username: user.username})
+        |> Ash.run_action()
+
+      assert {:ok, user_result} = result
+      assert profile_photo.id == user_result.profile_photo.id
+    end
+
+    test "Can read user by id",
          %{
            user: user
          } do
-      assert {:ok, _} = {:ok, []}
+      result =
+        FastUser
+        |> Ash.ActionInput.for_action(:by_id_email_or_username, %{id: user.id})
+        |> Ash.run_action()
+
+      assert {:ok, user_result} = result
+      assert user.zip_code == user_result.city.zip_code
+      assert 0 == user_result.credit_points
+    end
+
+    test "Can read user by email",
+         %{
+           user: user,
+           user_role: user_role
+         } do
+      result =
+        FastUser
+        |> Ash.ActionInput.for_action(:by_id_email_or_username, %{email: user.email})
+        |> Ash.run_action()
+
+      assert {:ok, user_result} = result
+      assert [] != Enum.filter(user_result.roles, fn role -> role.id == user_role.id end)
     end
   end
 
   defp create_user do
     {:ok, user} =
-      BasicUser.create(%{
+      User.create(%{
         email: "bob@example.com",
         username: "bob",
         name: "Bob",
@@ -67,13 +105,16 @@ defmodule Animina.Accounts.FastUserTest do
 
     file_path_without_uploads = String.replace(file_path, "uploads/", "")
 
-    Photo.create(%{
-      user_id: user_id,
-      filename: file_path_without_uploads,
-      original_filename: file_path_without_uploads,
-      size: 100,
-      ext: "jpg",
-      mime: "image/jpeg"
-    })
+    {:ok, photo} =
+      Photo.create(%{
+        user_id: user.id,
+        filename: file_path_without_uploads,
+        original_filename: file_path_without_uploads,
+        size: 100,
+        ext: "jpg",
+        mime: "image/jpeg"
+      })
+
+    photo
   end
 end
