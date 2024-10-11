ce42a1842eb76bb4cadbc19975a9e440b3427909
briankariuki <briankariukidev@gmail.com>
2024-10-11 09:21:10 +0300

    remove constraints, attributes from FastUser resource

--- lib/animina/accounts/resources/fast_user.ex
+++ lib/animina/accounts/resources/fast_user.ex
@@ -199,88 +199,31 @@ defmodule Animina.Accounts.FastUser do
 
   attributes do
     uuid_primary_key :id
-    attribute :email, :ci_string, allow_nil?: false, public?: true
-    attribute :hashed_password, :string, allow_nil?: false, sensitive?: true
+    attribute :email, :ci_string, public?: true
     attribute :is_in_waitlist, :boolean, default: false, public?: true
-
-    attribute :username, :ci_string do
-      allow_nil? false
-      public? true
-
-      constraints max_length: 15,
-                  min_length: 2,
-                  match: ~r/^[A-Za-z0-9._-]*$/,
-                  trim?: true,
-                  allow_empty?: false
-    end
-
-    attribute :name, :string do
-      allow_nil? false
-      public? true
-
-      constraints max_length: 50,
-                  min_length: 1,
-                  trim?: true,
-                  allow_empty?: false
-    end
-
-    attribute :birthday, :date, allow_nil?: false, public?: true
-
-    attribute :zip_code, :string do
-      constraints trim?: true,
-                  allow_empty?: false
-    end
-
-    attribute :country, :string do
-      allow_nil? false
-      public? true
-    end
-
-    attribute :gender, :string, allow_nil?: false, public?: true
-
-    attribute :height, :integer do
-      allow_nil? false
-      public? true
-
-      constraints max: 250,
-                  min: 40
-    end
-
-    attribute :mobile_phone, :ash_phone_number, allow_nil?: false
-
-    attribute :minimum_partner_height, :integer, allow_nil?: true
-    attribute :maximum_partner_height, :integer, allow_nil?: true
-
-    attribute :minimum_partner_age, :integer do
-      allow_nil? true
-      constraints min: 18
-    end
-
-    attribute :maximum_partner_age, :integer, allow_nil?: true, public?: true
-
-    attribute :partner_gender, :string, allow_nil?: true, public?: true
-
-    attribute :search_range, :integer, allow_nil?: true, public?: true
-    attribute :language, :string, allow_nil?: true, public?: true
-    attribute :legal_terms_accepted, :boolean, default: false, public?: true
-    attribute :registration_completed_at, :utc_datetime_usec, allow_nil?: true, public?: true
-    attribute :preapproved_communication_only, :boolean, default: false, public?: true
+    attribute :username, :ci_string, public?: true
+    attribute :name, :string, public?: true
+    attribute :birthday, :date, public?: true
+    attribute :zip_code, :string, public?: true
+    attribute :country, :string, public?: true
+    attribute :gender, :string, public?: true
+    attribute :height, :integer, public?: true
+    attribute :mobile_phone, :ash_phone_number, public?: true
+    attribute :minimum_partner_height, :integer, public?: true
+    attribute :maximum_partner_height, :integer, public?: true
+    attribute :minimum_partner_age, :integer, public?: true
+    attribute :maximum_partner_age, :integer, public?: true
+    attribute :partner_gender, :string, public?: true
+    attribute :search_range, :integer, public?: true
+    attribute :language, :string, public?: true
+    attribute :legal_terms_accepted, :boolean, public?: true
+    attribute :registration_completed_at, :utc_datetime_usec, public?: true
+    attribute :preapproved_communication_only, :boolean, public?: true
     attribute :streak, :integer, default: 0, public?: true
-    attribute :confirmed_at, :utc_datetime_usec, allow_nil?: true, public?: true
-
-    attribute :last_registration_page_visited, :string,
-      allow_nil?: true,
-      public?: true,
-      default: "/my/potential-partner"
-
-    attribute :occupation, :string do
-      constraints max_length: 40,
-                  trim?: true,
-                  allow_empty?: false
-    end
-
-    attribute :is_private, :boolean, default: false, public?: true
-
+    attribute :confirmed_at, :utc_datetime_usec, public?: true
+    attribute :last_registration_page_visited, :string, public?: true
+    attribute :occupation, :string, public?: true
+    attribute :is_private, :boolean, public?: true
     attribute :roles, {:array, :map}, public?: true, generated?: true
     attribute :city, :map, public?: true, generated?: true
     attribute :profile_photo, :map, public?: true, generated?: true
