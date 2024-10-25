759342e16b11e18bdb5405d66c5e1c5e12f0f16c
briankariuki <briankariukidev@gmail.com>
2024-10-25 13:02:03 +0300

    add list users action to FastUser resource

--- lib/animina/accounts/resources/fast_user.ex
+++ lib/animina/accounts/resources/fast_user.ex
@@ -27,11 +27,60 @@ defmodule Animina.Accounts.FastUser do
 
   code_interface do
     domain Animina.Accounts
-    define :by_id_email_or_username, args: [:id, :username, :email]
+    define :by_id_email_or_username
+    define :list
   end
 
   actions do
     defaults []
+
+    action :list, :map do
+      argument :limit, :integer, default: 10
+      argument :page, :integer, default: 1, constraints: [min: 1]
+
+      run fn input, _ ->
+        # we dont need to filter by id, username or email. So we pass true as filter
+        query = build_query(true, true, true)
+
+        # query pagination
+        limit = input.arguments.limit
+        offset = (input.arguments.page - 1) * input.arguments.limit
+
+        # total users query
+        total_count_query =
+          from u in user_query(),
+            select: count(u.id)
+
+        # merge pagination and count to query
+        query =
+          from u in query,
+            limit: ^limit,
+            offset: ^offset,
+            select_merge: %{count: subquery(total_count_query)}
+
+        # load the results
+        results =
+          Repo.all(query)
+
+        # get count of total results
+        count = Enum.at(results, 0, %{}) |> Map.get(:count)
+
+        # map results to user struct
+        results = Enum.map(results, fn user -> to_user_struct(user) end)
+
+        results =
+          struct(Ash.Page.Offset, %{
+            count: count,
+            results: results,
+            offset: offset,
+            more?: count > input.arguments.page * input.arguments.limit,
+            page: input.arguments.page,
+            limit: input.arguments.limit
+          })
+
+        {:ok, results}
+      end
+    end
 
     action :by_id_email_or_username, :map do
       argument :id, :string, public?: true
@@ -39,41 +88,6 @@ defmodule Animina.Accounts.FastUser do
       argument :email, :string, public?: true
 
       run fn input, _ ->
-        # user to ecto query
-        {:ok, query} =
-          Ash.Query.new(__MODULE__)
-          |> Ash.Query.data_layer_query()
-
-        # roles to ecto query
-        {:ok, roles_query} =
-          Ash.Query.new(Role)
-          |> Ash.Query.data_layer_query()
-
-        # user roles to ecto query
-        {:ok, user_roles_query} =
-          Ash.Query.new(UserRole)
-          |> Ash.Query.data_layer_query()
-
-        # city to ecto query
-        {:ok, city_query} =
-          Ash.Query.new(City)
-          |> Ash.Query.data_layer_query()
-
-        # photo to ecto query
-        {:ok, photo_query} =
-          Ash.Query.new(Photo)
-          |> Ash.Query.data_layer_query()
-
-        # optimized photo to ecto query
-        {:ok, optimized_photo_query} =
-          Ash.Query.new(OptimizedPhoto)
-          |> Ash.Query.data_layer_query()
-
-        # credit points to ecto query
-        {:ok, credit_points_query} =
-          Ash.Query.new(Credit)
-          |> Ash.Query.data_layer_query()
-
         # build where filter
         filter_by_id =
           if input.arguments[:id] != nil do
@@ -96,70 +110,7 @@ defmodule Animina.Accounts.FastUser do
             true
           end
 
-        query =
-          from u in query,
-            where: ^filter_by_username,
-            where: ^filter_by_id,
-            where: ^filter_by_email,
-            left_join: ur in subquery(user_roles_query),
-            on: u.id == ur.user_id,
-            left_join: r in subquery(roles_query),
-            on: ur.role_id == r.id,
-            left_join: c in subquery(city_query),
-            on: u.zip_code == c.zip_code,
-            left_join: p in subquery(photo_query),
-            on: u.id == p.user_id and is_nil(p.story_id),
-            left_join: op in subquery(optimized_photo_query),
-            on: p.id == op.photo_id,
-            left_join: cr in subquery(credit_points_query),
-            on: u.id == cr.user_id,
-            group_by: [
-              u.id,
-              c.id,
-              c.name,
-              c.zip_code,
-              c.lat,
-              c.lon,
-              p.id,
-              p.filename,
-              p.original_filename,
-              p.state
-            ],
-            select: %{
-              u
-              | roles:
-                  fragment(
-                    "json_agg(DISTINCT jsonb_build_object('id', ?, 'user_id', ?, 'role_id', ?, 'role', json_build_object('id', ?, 'name', ?)))",
-                    ur.id,
-                    ur.user_id,
-                    ur.role_id,
-                    r.id,
-                    r.name
-                  ),
-                city:
-                  fragment(
-                    "json_build_object('id', ?, 'name', ?, 'zip_code', ?, 'lat', ?, 'lon', ?)",
-                    c.id,
-                    c.name,
-                    c.zip_code,
-                    c.lat,
-                    c.lon
-                  ),
-                profile_photo:
-                  fragment(
-                    "json_build_object('id', ?, 'filename', ?, 'original_filename', ?, 'state', ?, 'optimized_photos', json_agg(DISTINCT jsonb_build_object('id', ?, 'image_url', ?, 'type', ?, 'user_id', ?, 'photo_id', ?)))",
-                    p.id,
-                    p.filename,
-                    p.original_filename,
-                    p.state,
-                    op.id,
-                    op.image_url,
-                    op.type,
-                    op.user_id,
-                    op.photo_id
-                  ),
-                credit_points: coalesce(sum(cr.points), 0) |> type(:integer)
-            }
+        query = build_query(filter_by_id, filter_by_username, filter_by_email)
 
         # load the results
         result =
@@ -168,34 +119,9 @@ defmodule Animina.Accounts.FastUser do
         if is_nil(result) do
           {:error, :user_not_found}
         else
-          # cast roles, city, profile_photo to struct
-          roles =
-            Enum.map(result.roles, fn user_role ->
-              user_role = struct(UserRole, Animina.keys_to_atoms(user_role))
+          user = to_user_struct(result)
 
-              Map.merge(user_role, %{
-                role: struct(Role, user_role.role)
-              })
-            end)
-
-          city = struct(City, Animina.keys_to_atoms(result.city))
-          profile_photo = struct(Photo, Animina.keys_to_atoms(result.profile_photo))
-          age = UserAge.calculate_age(result.birthday)
-
-          optimized_photos =
-            Enum.map(profile_photo.optimized_photos, fn optimized_photo ->
-              struct(OptimizedPhoto, optimized_photo)
-            end)
-
-          result =
-            Map.merge(result, %{
-              roles: roles,
-              age: age,
-              city: city,
-              profile_photo: Map.merge(profile_photo, %{optimized_photos: optimized_photos})
-            })
-
-          {:ok, result}
+          {:ok, user}
         end
       end
     end
@@ -233,8 +159,167 @@ defmodule Animina.Accounts.FastUser do
     attribute :city, :map, public?: true, generated?: true
     attribute :profile_photo, :map, public?: true, generated?: true
     attribute :credit_points, :integer, public?: true, generated?: true
+    attribute :count, :integer, public?: true, generated?: true
 
     create_timestamp :created_at
     update_timestamp :updated_at
+  end
+
+  defp build_query(filter_by_id, filter_by_username, filter_by_email) do
+    query = user_query()
+    user_roles_query = user_roles_query()
+    roles_query = roles_query()
+    city_query = city_query()
+    photo_query = photo_query()
+    optimized_photo_query = optimized_photo_query()
+    credit_points_query = credit_points_query()
+
+    from u in query,
+      where: ^filter_by_username,
+      where: ^filter_by_id,
+      where: ^filter_by_email,
+      left_join: ur in subquery(user_roles_query),
+      on: u.id == ur.user_id,
+      left_join: r in subquery(roles_query),
+      on: ur.role_id == r.id,
+      left_join: c in subquery(city_query),
+      on: u.zip_code == c.zip_code,
+      left_join: p in subquery(photo_query),
+      on: u.id == p.user_id and is_nil(p.story_id),
+      left_join: op in subquery(optimized_photo_query),
+      on: p.id == op.photo_id,
+      left_join: cr in subquery(credit_points_query),
+      on: u.id == cr.user_id,
+      group_by: [
+        u.id,
+        c.id,
+        c.name,
+        c.zip_code,
+        c.lat,
+        c.lon,
+        p.id,
+        p.filename,
+        p.original_filename,
+        p.state
+      ],
+      select: %{
+        u
+        | roles:
+            fragment(
+              "json_agg(DISTINCT jsonb_build_object('id', ?, 'user_id', ?, 'role_id', ?, 'role', json_build_object('id', ?, 'name', ?)))",
+              ur.id,
+              ur.user_id,
+              ur.role_id,
+              r.id,
+              r.name
+            ),
+          city:
+            fragment(
+              "json_build_object('id', ?, 'name', ?, 'zip_code', ?, 'lat', ?, 'lon', ?)",
+              c.id,
+              c.name,
+              c.zip_code,
+              c.lat,
+              c.lon
+            ),
+          profile_photo:
+            fragment(
+              "json_build_object('id', ?, 'filename', ?, 'original_filename', ?, 'state', ?, 'optimized_photos', json_agg(DISTINCT jsonb_build_object('id', ?, 'image_url', ?, 'type', ?, 'user_id', ?, 'photo_id', ?)))",
+              p.id,
+              p.filename,
+              p.original_filename,
+              p.state,
+              op.id,
+              op.image_url,
+              op.type,
+              op.user_id,
+              op.photo_id
+            ),
+          credit_points: coalesce(sum(cr.points), 0) |> type(:integer)
+      }
+  end
+
+  defp to_user_struct(user) do
+    # cast roles, city, profile_photo to struct
+    roles =
+      Enum.map(user.roles, fn user_role ->
+        user_role = struct(UserRole, Animina.keys_to_atoms(user_role))
+
+        Map.merge(user_role, %{
+          role: struct(Role, user_role.role)
+        })
+      end)
+
+    city = struct(City, Animina.keys_to_atoms(user.city))
+    profile_photo = struct(Photo, Animina.keys_to_atoms(user.profile_photo))
+    age = UserAge.calculate_age(user.birthday)
+
+    optimized_photos =
+      Enum.map(profile_photo.optimized_photos, fn optimized_photo ->
+        struct(OptimizedPhoto, optimized_photo)
+      end)
+
+    Map.merge(user, %{
+      roles: roles,
+      age: age,
+      city: city,
+      profile_photo: Map.merge(profile_photo, %{optimized_photos: optimized_photos})
+    })
+  end
+
+  defp user_query do
+    {:ok, query} =
+      Ash.Query.new(__MODULE__)
+      |> Ash.Query.data_layer_query()
+
+    query
+  end
+
+  defp roles_query do
+    {:ok, query} =
+      Ash.Query.new(Role)
+      |> Ash.Query.data_layer_query()
+
+    query
+  end
+
+  defp user_roles_query do
+    {:ok, query} =
+      Ash.Query.new(UserRole)
+      |> Ash.Query.data_layer_query()
+
+    query
+  end
+
+  defp city_query do
+    {:ok, query} =
+      Ash.Query.new(City)
+      |> Ash.Query.data_layer_query()
+
+    query
+  end
+
+  defp photo_query do
+    {:ok, query} =
+      Ash.Query.new(Photo)
+      |> Ash.Query.data_layer_query()
+
+    query
+  end
+
+  defp optimized_photo_query do
+    {:ok, query} =
+      Ash.Query.new(OptimizedPhoto)
+      |> Ash.Query.data_layer_query()
+
+    query
+  end
+
+  defp credit_points_query do
+    {:ok, query} =
+      Ash.Query.new(Credit)
+      |> Ash.Query.data_layer_query()
+
+    query
   end
 end
