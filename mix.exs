a7aaabf7562cc0c25e05e7b93c251deeee9d98b9
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2026-01-29 18:04:37 +0100

    Bump version to 2.0.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Animina.MixProject do
   def project do
     [
       app: :animina,
-      version: "2.0.1",
+      version: "2.0.2",
       elixir: "~> 1.19",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
