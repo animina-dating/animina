55119c1f30e60d68423eca328b1faa64101d74b0
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2026-01-29 15:53:43 +0100

    Set 2.0.0 version for the new application

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Animina.MixProject do
   def project do
     [
       app: :animina,
-      version: "0.1.0",
+      version: "2.0.0",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
